5a0ae9f4e926991293c0757328099b5feb2da098
Dean Balandin <dbalandin@microsoft.com>
2023-08-25 18:50:40 +0000

    support alternative templates

--- play.sh
+++ play.sh
@@ -1,23 +1,63 @@
 #!/bin/bash
 set -e
 
+
+
+#!/bin/bash
+set -e
+
+template="go-dev"
+# Loop over options
+while getopts "t:" opt; do
+  case $opt in
+    t) # Option t expects an argument
+      # Check if the argument is missing or another option
+      if [[ -z $OPTARG || $OPTARG == -* ]]; then
+        echo "Option -t requires an argument" >&2
+        exit 1
+      else
+        # Assign the argument to the variable
+        template=$OPTARG
+      fi
+      ;;
+    \?) # Invalid option
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :) # Missing option argument
+      echo "Option -$OPTARG requires an argument" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Shift the positional parameters to skip the options
+shift $((OPTIND - 1))
+
 readonly rootdir=$(dirname "$(realpath $0)")
-readonly PROJECT_NAME=${1:-"testproj"}
-readonly PROJECT_DIR="$rootdir/projects/$PROJECT_NAME"
+readonly template_dir="$rootdir/templates/$template"
+readonly project_name=${1:-"testproj"}
+readonly project_dir="$rootdir/projects/$project_name"
+
+
+if [ ! -d "$template_dir" ]; then
+    echo "Template $template does not exist"
+    exit 1
+fi
 
 # Make sure projects directory exists
 mkdir -p "$rootdir/projects"
 
-
-if [ ! -d "$PROJECT_DIR" ]; then
-    echo "Creating project $PROJECT_NAME"
-    cp -ra $rootdir/templates/go-dev $PROJECT_DIR
-    sed -i "s/<PROJECT_NAME>/$PROJECT_NAME/g" "$PROJECT_DIR/.devcontainer/docker-compose.yaml"
+if [ ! -d "$project_dir" ]; then
+        echo "Creating project $project_name"
+        cp -ra $template_dir $project_dir
+        sed -i "s/<PROJECT_NAME>/$project_name/g" "$project_dir/.devcontainer/docker-compose.yaml"
 else
-    echo "Project $PROJECT_NAME already exists. Will open it now"
+        echo "Project $project_name already exists. Will open it now."
 fi
 
-code $PROJECT_DIR
+code "$project_dir"
+
 
 # function install_devcontainer_cli() {
 #     if command -v devcontainer &> /dev/null
@@ -40,7 +80,7 @@ code $PROJECT_DIR
 
 # install_devcontainer_cli
 
-# containerID=$(devcontainer up --workspace-folder $PROJECT_DIR | jq ".containerId" -r)
-# code --folder-uri "vscode-remote://dev-container+$containerID$PROJECT_DIR"
+# containerID=$(devcontainer up --workspace-folder $project_dir | jq ".containerId" -r)
+# code --folder-uri "vscode-remote://dev-container+$containerID$project_dir"
 
 # Open code and manually rebuild and reopen containercode --folder-uri vscode-remote://dev-container+file://<path-to-folder>
